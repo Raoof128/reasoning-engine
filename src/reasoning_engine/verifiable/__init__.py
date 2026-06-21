e6678b9befffeab3be3be00182fcd93cf0ac382f
Raoof128 <raoof.r12@gmail.com>
2026-06-21 19:04:30 +1000

    feat(research): add verifiable research models

---
+++ src/reasoning_engine/verifiable/__init__.py
@@ -0,0 +1,17 @@
+"""Verifiable research engine components."""
+
+from reasoning_engine.verifiable.models import (
+    ClaimRecord,
+    EvidenceRecord,
+    ResearchRun,
+    RetrievalError,
+    VerificationRecord,
+)
+
+__all__ = [
+    "ClaimRecord",
+    "EvidenceRecord",
+    "ResearchRun",
+    "RetrievalError",
+    "VerificationRecord",
+]
